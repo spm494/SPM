800c5f5ed348049c638339d810f90542f304b38c
spm494 <spmproject71@gmail.com>
2026-03-02 16:15:43 +0530

    Create square.py

---
+++ square.py
@@ -0,0 +1,9 @@
+rows = 5
+
+for i in range(rows):
+    for j in range(rows):
+        if i == 0 or i == rows-1 or j == 0 or j == rows-1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
